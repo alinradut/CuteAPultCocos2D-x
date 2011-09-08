da955a5ef0d4c6627b120acdb85699c597aa3e7b
clawoo <alin.claudiu.radut@gmail.com>
2011-09-08 23:35:01 +0300

    Clean up the code as per the 'Cleaning Up' chapter

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -20,7 +20,6 @@ enum
 HelloWorld::HelloWorld()
 {
 	setIsTouchEnabled( true );
-	setIsAccelerometerEnabled( true );
     
 	CCSize screenSize = CCDirector::sharedDirector()->getWinSize();
 	//UXLOG(L"Screen width %0.2f screen height %0.2f",screenSize.width,screenSize.height);
@@ -78,19 +77,6 @@ HelloWorld::HelloWorld()
 	groundBox.SetAsEdge(b2Vec2(screenSize.width/PTM_RATIO,screenSize.height/PTM_RATIO), b2Vec2(screenSize.width/PTM_RATIO,0));
 	groundBody->CreateFixture(&groundBox, 0);
     
-	
-	//Set up sprite
-	
-	CCSpriteBatchNode *mgr = CCSpriteBatchNode::batchNodeWithFile("blocks.png", 150);
-	addChild(mgr, 0, kTagSpriteManager);
-	
-	addNewSpriteWithCoords( CCPointMake(screenSize.width/2, screenSize.height/2) );
-	
-	CCLabelTTF *label = CCLabelTTF::labelWithString("Tap screen", "Marker Felt", 32);
-	addChild(label, 0);
-	label->setColor( ccc3(0,0,255) );
-	label->setPosition( CCPointMake( screenSize.width/2, screenSize.height-50) );
-	
 	schedule( schedule_selector(HelloWorld::tick) );
 }
 
@@ -119,41 +105,6 @@ void HelloWorld::draw()
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);	
 }
 
-void HelloWorld::addNewSpriteWithCoords(CCPoint p)
-{
-	//UXLOG(L"Add sprite %0.2f x %02.f",p.x,p.y);
-	CCSpriteBatchNode* sheet = (CCSpriteBatchNode*)getChildByTag(kTagSpriteManager);
-	
-	//We have a 64x64 sprite sheet with 4 different 32x32 images.  The following code is
-	//just randomly picking one of the images
-	int idx = (CCRANDOM_0_1() > .5 ? 0:1);
-	int idy = (CCRANDOM_0_1() > .5 ? 0:1);
-	CCSprite *sprite = CCSprite::spriteWithBatchNode(sheet, CCRectMake(32 * idx,32 * idy,32,32));
-	sheet->addChild(sprite);
-	
-	sprite->setPosition( CCPointMake( p.x, p.y) );
-	
-	// Define the dynamic body.
-	//Set up a 1m squared box in the physics world
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(p.x/PTM_RATIO, p.y/PTM_RATIO);
-	bodyDef.userData = sprite;
-	b2Body *body = world->CreateBody(&bodyDef);
-	
-	// Define another box shape for our dynamic body.
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(.5f, .5f);//These are mid points for our 1m box
-	
-	// Define the dynamic body fixture.
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;	
-	fixtureDef.density = 1.0f;
-	fixtureDef.friction = 0.3f;
-	body->CreateFixture(&fixtureDef);
-}
-
-
 void HelloWorld::tick(ccTime dt)
 {
 	//It is recommended that a fixed time step is used with Box2D for stability
@@ -177,27 +128,6 @@ void HelloWorld::tick(ccTime dt)
 			myActor->setPosition( CCPointMake( b->GetPosition().x * PTM_RATIO, b->GetPosition().y * PTM_RATIO) );
 			myActor->setRotation( -1 * CC_RADIANS_TO_DEGREES(b->GetAngle()) );
 		}	
-	}
-}
-
-void HelloWorld::ccTouchesEnded(CCSet* touches, CCEvent* event)
-{
-	//Add a new body/atlas sprite at the touched location
-	CCSetIterator it;
-	CCTouch* touch;
-    
-	for( it = touches->begin(); it != touches->end(); it++) 
-	{
-		touch = (CCTouch*)(*it);
-        
-		if(!touch)
-			break;
-        
-		CCPoint location = touch->locationInView(touch->view());
-		
-		location = CCDirector::sharedDirector()->convertToGL(location);
-        
-		addNewSpriteWithCoords( location );
 	}
 }
 
